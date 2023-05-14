eb58d3b11ae1375e09b57b32779de790b7bb0f91
StarryBadger <shaunak.biswas@research.iiit.ac.in>
2023-05-14 16:14:26 +0530

    Added time bias

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 import imaplib
 import email
 from email.header import decode_header
-import webbrowser
-import os
+from datetime import datetime
 
 username = "throwaway2441139@outlook.com"
 password = "telegrammemymail@github"
@@ -14,7 +13,7 @@ status, messages = imap.select("INBOX")
 N = 3
 messages = int(messages[0])
 
-for i in range(messages, messages-N, -1):
+for i in range(messages, 0, -1):
     res, msg = imap.fetch(str(i), "(RFC822)")
     for response in msg:
         if isinstance(response, tuple):
@@ -22,10 +21,22 @@ for i in range(messages, messages-N, -1):
             subject, encoding = decode_header(msg["Subject"])[0]
             if isinstance(subject, bytes):
                 subject = subject.decode(encoding)
+            date, encoding = decode_header(msg["Date"])[0]
+            if isinstance(date, bytes):
+                date = date.decode(encoding)
+            if i==messages:
+                newestdate=date
+            date = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %z")
+            with open('prevtime.txt', 'r') as file:
+                prevDate = file.read()
+            prevDate = datetime.strptime(prevDate, "%a, %d %b %Y %H:%M:%S %z")
+            if prevDate>=date:
+                break
             From, encoding = decode_header(msg.get("From"))[0]
             if isinstance(From, bytes):
                 From = From.decode(encoding)
             print("Subject:", subject)
+            print("Time: ",date)
             print("From:", From)
             if msg.is_multipart():
                 for part in msg.walk():
@@ -38,6 +49,8 @@ for i in range(messages, messages-N, -1):
                     if content_type == "text/plain" and "attachment" not in content_disposition:
                         print(body)
                     elif "attachment" in content_disposition:
+                        body="Image attachment detected"
+                        print(body)
                         filepath="./img.png"
                         open(filepath, "wb").write(part.get_payload(decode=True))
             else:
@@ -45,5 +58,7 @@ for i in range(messages, messages-N, -1):
                 body = msg.get_payload(decode=True).decode()
                 if content_type == "text/plain":
                     print(body)
+with open('prevtime.txt', 'w') as file:
+    file.write(newestdate)
 imap.close()
 imap.logout()
